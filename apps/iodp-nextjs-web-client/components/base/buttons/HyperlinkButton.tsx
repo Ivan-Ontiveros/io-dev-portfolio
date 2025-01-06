5274c883be52c211802e8e5420d4e66ee21dcbd7
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2025-01-06 08:59:43 -0700

    feat(components): create Tile & WebPage components + add 'className' prop to HyperlinkButton component

--- apps/iodp-nextjs-web-client/components/base/buttons/HyperlinkButton.tsx
+++ apps/iodp-nextjs-web-client/components/base/buttons/HyperlinkButton.tsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 type HyperlinkButtonProps = {
+  className?: string;
   url: string;
   label: string;
   isExternal?: boolean;
 };
 
 export const HyperlinkButton = ({
+  className,
   url,
   label,
   isExternal = false,
 }: HyperlinkButtonProps) => (
-  <Button asChild>
+  <Button asChild className={className ? className : ""}>
     <Link
       href={url}
       target={isExternal ? "_blank" : undefined}

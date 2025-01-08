b193df898d2c27fde584f4eeb805e187393133a8
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2025-01-08 01:03:08 -0700

    refactor(metadata): update metadata description + fix site-brand content

--- apps/iodp-nextjs-web-client/components/blocks/AppHeader.tsx
+++ apps/iodp-nextjs-web-client/components/blocks/AppHeader.tsx
@@ -29,7 +29,9 @@ export const AppHeader = () => {
           <span className="brand-name" aria-label="Candidate's name">
             Ivan Ontiveros
           </span>
-          <span className="title">Software Engineer at Health Company</span>
+          <span className="title">
+            Software Engineer at Aetna, a CVS Health Company
+          </span>
           <span className="location">Arizona, United States</span>
         </Link>
 
